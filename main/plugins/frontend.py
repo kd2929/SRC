e545cca1a1706bda8b8ae273010831f77bd3e34c
DRAGO <88723798+Drago-Owner@users.noreply.github.com>
2024-05-06 15:06:03 +0530

    Update frontend.py

--- main/plugins/frontend.py
+++ main/plugins/frontend.py
@@ -95,11 +95,11 @@ async def clone(event):
                 m = msg_id
                 await get_msg(userbot, Bot, event.sender_id, edit.id, link, m, file_name)
         except FloodWait as fw:
-            await Invix.send_message(event.sender_id, f'Try again after {fw.value} seconds due to floodwait from telegram.')
+            await ASUR.send_message(event.sender_id, f'Try again after {fw.value} seconds due to floodwait from telegram.')
             await edit.delete()
         except Exception as e:
             logging.info(e)
-            await Invix.send_message(event.sender_id, f"An error occurred during cloning of `{link}`\n\n**Error:** {str(e)}")
+            await ASUR.send_message(event.sender_id, f"An error occurred during cloning of `{link}`\n\n**Error:** {str(e)}")
             await edit.delete()
         ind = user.index(f'{int(event.sender_id)}')
         user.pop(int(ind))
